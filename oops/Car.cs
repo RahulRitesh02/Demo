6410b374e1cc890db981e3ec708633d02a917edf
Rahul <riteshrahul94@gmail.com>
2021-02-02 21:24:35 +0530

    initial commit

---
+++ oops/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    public class Car
+    {
+        int a;
+        public int _newA { get; set; }
+        public int _newB { get; set; }
+        public Car(int a,int b)
+        {
+            _newA = a;
+            _newB = b;
+
+        }
+        public string stering { get; set; }
+        public int Wheel { get; set; }
+
+        public Int32 headlight { get;  set; }
+    }
+}
---
+++ oops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car obj = new Car(10,20);
+            var a = obj._newA;
+            var b = obj._newB;
+        }
+    }
+}
